25eaa0fbd24430ff15260825e270887cae006137
Bob Smith <wasadmin@webpagesolutions.com>
2023-02-08 16:48:18 -0500

    search by full name functioning

--- server-backend/faker.js
+++ server-backend/faker.js
@@ -6,16 +6,14 @@ function generateEmployees(){
     let employees = [];
     console.log(faker);
     for(let i = 0; i <25; i++){
-        let firstName = faker.faker.name.firstName();
-        let lastName = faker.faker.name.lastName();
+        let name = faker.faker.name.fullName();
         let phoneNumber = faker.faker.phone.phoneNumber();
         let jobRole = faker.faker.name.jobType();
         let location = faker.faker.address.cityName();
         let salary = faker.faker.finance.amount(80000, 300000, 2);
         employees.push({
             id: i,
-            first_name: firstName,
-            last_name: lastName,
+            name: name,
             phone_number: phoneNumber,
             job_role: jobRole,
             location: location,
