088befbd5018cb2f17ba6979ca256935fadd17d3
so and so <soandso@was.com>
2023-02-08 16:17:11 -0500

    update

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { BrowserRouter as 
-  BrowserRouter,
-  Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter, Link} from "react-router-dom";
 import {Button, Row, Col, ButtonGroup} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import background from "./images/background.png"
+import './App.css';
 import EmployeeContainer from "./components/EmployeeContainer"
 import Header from "./components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Row styles={styles.wrapper}>
-        <header style={styles.main}>
-          <h4>HEADER</h4>
+      <Row style={styles.wrapper} className=" img-responsive">
+        <header style={styles.header}>
+          <h4>Employee Directory</h4>
         </header>
         <main style={styles.main}>
-          <h2>Main Page</h2>
+          <h2>MainBody</h2>
         </main>
         <footer style={styles.footer}>
           Copyright &copy; kmj.com {new Date().getUTCFullYear()}
@@ -34,8 +29,6 @@ export default function App() {
     wrapper:{
       backgroundImage: `url(${background})`,
       backgroundPosition: "center",
-      //backgroundRepeat: "repeat",
-      //overflowX: "scroll",
       height: "100vh"
     }, 
     header:{
@@ -43,7 +36,8 @@ export default function App() {
       height: "5%"
     },
     main:{
-      height: "90%"
+      height: "90%",
+      textPosition: "center"
     },
     footer:{
       position: "absolute",
