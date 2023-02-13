f2325cd8e6705fcc1e2af75445ff2eb493a2d465
Kay Edwards <kyedward@travelers.com>
2023-02-13 05:13:37 -0500

    login page

--- client-frontend/src/components/Auth/index.js
+++ client-frontend/src/components/Auth/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import background from "../../images/background.png";
+import {Button} from 'react-bootstrap';
 
 async function loginUser(credentials) {
   return fetch('http://localhost:5000/users/authenticate', {
@@ -28,43 +30,56 @@ export default function Login({setLoggedInUser}) {
     setLoggedInUser(token);
   }
   return(
+    <div  style={styles.bgDiv}>
     <div className="login-wrapper" style={styles.loginWrapper}>
-      <h1>Please Log In to View the Employee Directory</h1>
+      <h1>Enterprise Employee Directory</h1>
+      <h4>Login</h4>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label style={styles.labelDiv}>
           <p>Username</p>
           <input type="text" style={styles.input} onChange={e => setUserName(e.target.value)} />
         </label>
-        <label>
+        <label style={styles.labelDiv}> 
           <p>Password</p>
           <input type="password" style={styles.input} onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
-          <button type="submit" style={styles.button}>Submit</button>
+          <Button type="submit"  variant="danger" style={styles.button}>Submit</Button>
         </div>
       </form>
+    </div>
     </div>
   )
 }
 
 
 const styles = {
+  bgDiv: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center",
+    height: "100vh",
+    width: "100vw",
+    backgroundSize: "cover"
+  },
   loginWrapper: {
     margin: "2px",
-    padding: '2px',
+    padding: '200px',
     textAlign: 'center'
+  },
+  labelDiv: {
+    fontWeight: 'bold'
   },
   input: {
     margin: '2px',
     padding: '5px',
-    borderRadius: '5px'
+    borderRadius: '5px',
+    border: '2px red solid'
   },
   button: {
     margin: '2px',
     padding: '5px',
     borderRadius: '5px',
-    backgroundColor: '#E5E4E2',
-    border: '2px black solid',
+    border: '2px red solid',
     fontWeight: 'bold'
   }
 }
